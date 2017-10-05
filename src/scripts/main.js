faa0651ed9d572ca5697882baee8410e5dbccecf
Aditya Rao <dev.adityarao@gmail.com>
2017-10-05 17:09:35 +0530

    Small changes in css and javascript files.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,21 @@
 //  Make your HTML 'hot reload' using 'DefinePlugin' & 'html-loader'.
 if (process.env.NODE_ENV !== 'production') {
-  // index.html is relative to main.js
+// index.html is relative to main.js
   require('../index.html')
 }
 
 // Require|import the CSS file from styles folder to inject it into index.html automatically.
 import "../styles/main.css";
 
-var rick = document.getElementById('alien_rick');
-rick.addEventListener('click',function(){
-  var happyrick = document.getElementById('dance');
-  happyrick.style.display = "block"; 
+var firstRick = document.getElementById('alien_rick_one');
+var secondRick = document.getElementById('alien_rick_two');
+
+firstRick.addEventListener('click',function(){
+  secondRick.style.display = "block";
+  firstRick.style.display = "none"; 
 });
 
+secondRick.addEventListener('click',function(){
+  firstRick.style.display = "block"; 
+  secondRick.style.display = "none";
+});
